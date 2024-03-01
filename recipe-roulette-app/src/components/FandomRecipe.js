4dca5dff77697fdc77d5f9d5900cb5fecbb2665a
Jana2xN <jana.neirinck@hotmail.com>
2024-03-01 15:43:41 +0100

    Add const FandomRecipe with return in FandomRecipe.js

--- recipe-roulette-app/src/components/FandomRecipe.js
+++ recipe-roulette-app/src/components/FandomRecipe.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+const FandomRecipe = ({ recipe }) => {
+    return (
+        <div>
+            <h2>{recipe.fandom}</h2>
+            <h3>{recipe.name}</h3>
+            <img src={require(`../images/${recipe.image}`).default} alt={recipe.name} />
+            <p>{recipe.instructions}</p>
+        </div>
+    );
+};
