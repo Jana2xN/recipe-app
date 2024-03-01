aae6ed9c15f4c7a8f3184aed16f6f163defbec2f
Jana2xN <jana.neirinck@hotmail.com>
2024-03-01 14:43:05 +0100

    Add const App and export default App

--- recipe-roulette-app/src/App.js
+++ recipe-roulette-app/src/App.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import FandomRecipeRoulette from './components/FandomRecipeRoulette';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Fandom Recipe Roulette</h1>
+      <FandomRecipeRoulette />
+    </div>
+  );
+};
+
+export default App;
