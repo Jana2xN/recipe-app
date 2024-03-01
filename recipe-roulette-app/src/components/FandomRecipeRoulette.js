7ddcbed9a0b21309f13a8d6372346bfefbe57ac1
Jana2xN <jana.neirinck@hotmail.com>
2024-03-01 15:38:50 +0100

    Add const FandomRecipeRoulette, const randomFandomRecipe and export default FandomRecipeRoulette; to FandomRecipeRoulette.js

--- recipe-roulette-app/src/components/FandomRecipeRoulette.js
+++ recipe-roulette-app/src/components/FandomRecipeRoulette.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import FandomRecipe from './FandomRecipe';
 import fandomRecipes from '../data/fandomRecipes';
+
+const FandomRecipeRoulette = () => {
+    const randomFandomRecipe = fandomRecipes[Math.floor(Math.random() * fandomRecipes.length)];
+
+    return (
+        <div>
+            <FandomRecipe recipe={randomFandomRecipe} />
+        </div>
+    );
+};
+
+export default FandomRecipeRoulette;
